f569fae4977fee3c17a6bd59c3f6349742784bb1
Brayan Marroquin <eduar.gol07@gmail.com>
2021-07-21 00:24:55 -0600

    Init commit

---
+++ src/routers/task.js
@@ -0,0 +1,103 @@
+const express = require('express')
+const router = new express.Router()
+const Task = require('../models/task')
+const auth = require('../middleware/auth')
+
+//GET /tasks?completed=true filtering data
+//GET /tasks?limit=2&skip=4 paginating data 
+//GET /tasks?sortBy=createdAt:desc Sorting Data
+router.get('/tasks', auth, async (req,res) => {
+    const match = {}
+    const sort = {}
+
+    if (req.query.completed){
+        match.completed = req.query.completed === 'true'
+    }
+
+    if (req.query.sortBy){
+        const parts = req.query.sortBy.split(':')
+        sort[parts[0]] = parts[1] === 'desc' ? -1 : 1
+    }
+
+    try {
+        await req.user.populate({
+            path: 'tasks',
+            match,
+            options: {
+                limit: parseInt(req.query.limit),
+                skip: parseInt(req.query.skip),
+                sort                 
+            }
+        }).execPopulate()
+
+        res.send(req.user.tasks)
+    } catch (e) {
+        res.status(500).send(e)
+    }
+})
+
+router.get('/tasks/:id', auth ,async (req,res) => {
+    const _id = req.params.id
+    try {
+        const task = await Task.findOne({ _id, owner: req.user._id })
+        if(!task){
+            return res.status(404).send()
+        }
+        res.send(task)
+    } catch (e) {
+        res.status(500).send(e)
+    }
+})
+
+router.post('/tasks', auth, async (req,res) => {   
+
+    console.log(req.body)
+    const task = new Task({
+        ...req.body,
+        owner: req.user._id
+    })
+
+    try {      
+        await task.save()
+        res.status(201).send(task)
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
+router.patch('/tasks/:id', auth, async(req,res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['description','completed']
+    const isValid = updates.every((upd) => allowedUpdates.includes(upd))
+
+    if(!isValid){
+        return res.status(400).send({'error':'Invalid updates values'})        
+    }
+
+    try {
+        const task = await Task.findOne({ _id: req.params.id, owner: req.user._id })
+        if(!task){
+            return res.status(404).send({'error':'No task found'})  
+        }
+        updates.forEach((attribute) => task[attribute] = req.body[attribute])
+        await task.save()
+        
+        res.status(200).send(task)
+    } catch (e) {
+        res.status(500).send(e)  
+    }
+})
+
+router.delete('/tasks/:id', auth, async (req, res) => {
+    try {
+        const task = await Task.findOneAndDelete({ _id: req.params.id, owner: req.user._id })
+        if(!task){
+            return res.status(404).send({'error':'No task found'})
+        }
+        res.status(200).send(task)
+    } catch (e) {
+        res.status(500).send(e) 
+    }
+})
+
+module.exports = router
